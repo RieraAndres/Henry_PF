40e927f65465b150cf812becd1848d3c3502a6d9
MarcosMoreno0912 <moreno83marcos@gmail.com>
2023-08-02 10:43:53 -0300

    Feature: create Models(User, Mascota, Adopcion y Donacion)

---
+++ Server/src/models/User.js
@@ -0,0 +1,51 @@
+{ DataTypes } = require('sequelize');
+{ v4: uuidv4 } = require('uuid');
+
+module.exports = sequelize.define('User', {
+	id: {
+		type: DataTypes.UUID,
+		primaryKey: true,
+	},
+	name: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	birthdate: {
+		type: DataTypes.DATEONLY, // FORMATO DE FECHA = Año/mes/día 
+		allowNull: false,
+	},
+	address: {
+		type: DataTypes.STRING,
+	},
+	email: {
+	 	type: DataTypes.STRING,
+	 	allowNull: false,
+	 	unique: true,
+	 	validate: {
+	 		isEmail: true,
+	 	},
+	},
+	userName: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		unique: true,
+	},
+	password: {   // Usar funciones para hashear o encriptar, y también para verificar 
+		type: DataTypes.STRING,   // librería bcrypt
+		allowNull: false,
+	},
+	numberPhone: {
+		type: DataTypes.STRING,
+	},
+	typeUser: {
+		type: DataTypes.ENUM('Adopter', 'Admin'),
+		allowNull: false,
+	},
+	donante: {
+		type: DataTypes.BOOLEAN,
+		defaultValue: false,
+	},
+	{
+		freezeTableName: true
+	}
+});
