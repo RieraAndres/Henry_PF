c79f095df418c8c876ef25ec53b751a06cbd99e6
Andres Riera <andresriera9999@gmail.com>
2023-08-02 17:43:15 -0300

    Creacion de Card Y Cards

--- Client/src/Components/Cards/cards.jsx
+++ Client/src/Components/Cards/cards.jsx
@@ -1,11 +1,29 @@
-import CardGroup from 'react-bootstrap/CardGroup';
-
-function cards() { //traigo las mascotas por props desde Home
-  const mascotas = [{name:"Pepito",img:"https://humanidades.com/wp-content/uploads/2017/02/perro-1-e1561678907722.jpg"}, {name:"Olaf",img:"https://upload.wikimedia.org/wikipedia/commons/0/04/Labrador_Retriever_%281210559%29.jpg"},{name:"Floki",img:"https://t1.ea.ltmcdn.com/es/posts/5/6/2/10_caracteristicas_de_los_perros_24265_600_square.jpg"}]
+import Card from "../Card/Card"
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import styles from '../Cards/Cards.module.css'
+function cards(/*{mascotas}*/) { //traigo las mascotas por props desde Home
+  
+  const mascotas = [
+    {name:"Pepito",img:"https://humanidades.com/wp-content/uploads/2017/02/perro-1-e1561678907722.jpg"},
+    {name:"Olaf",img:"https://upload.wikimedia.org/wikipedia/commons/0/04/Labrador_Retriever_%281210559%29.jpg"},
+    {name:"Floki",img:"https://t1.ea.ltmcdn.com/es/posts/5/6/2/10_caracteristicas_de_los_perros_24265_600_square.jpg"},
+    {name:"Floki",img:"https://t1.ea.ltmcdn.com/es/posts/5/6/2/10_caracteristicas_de_los_perros_24265_600_square.jpg"},
+    {name:"Floki",img:"https://t1.ea.ltmcdn.com/es/posts/5/6/2/10_caracteristicas_de_los_perros_24265_600_square.jpg"},
+    {name:"Floki",img:"https://t1.ea.ltmcdn.com/es/posts/5/6/2/10_caracteristicas_de_los_perros_24265_600_square.jpg"},
+    {name:"Floki",img:"https://t1.ea.ltmcdn.com/es/posts/5/6/2/10_caracteristicas_de_los_perros_24265_600_square.jpg"},
+    {name:"Floki",img:"https://t1.ea.ltmcdn.com/es/posts/5/6/2/10_caracteristicas_de_los_perros_24265_600_square.jpg"}
+  ]
 return (
-  <CardGroup> 
-    {mascotas.map((mascota)=><p>{mascota.name}</p>)}
-  </CardGroup>
+    <div className={styles.CardsContainer}>
+      <Row  xs={1} md={3} className="g-4">
+      {mascotas.map((mascota, idx) => (
+        <Col  key={idx}>
+          <Card mascota={mascota} />
+        </Col>
+      ))}
+    </Row>
+    </div>
        );
 }
 
