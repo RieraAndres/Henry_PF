99347b7dd9655c8f18ef402cd0808cd2cd513bc9
MarcosMoreno0912 <moreno83marcos@gmail.com>
2023-08-02 16:23:35 -0300

    Feature: create DataBase and password encrypts(User)

--- Server/src/models/User.js
+++ Server/src/models/User.js
@@ -1,51 +1,72 @@
 { DataTypes } = require('sequelize');
 { v4: uuidv4 } = require('uuid');
+const { hashPassword, comparePassword } = require('../passwordUtils/passwordUtils.js');
 
-module.exports = sequelize.define('User', {
-	id: {
-		type: DataTypes.UUID,
-		primaryKey: true,
-	},
-	name: {
-		type: DataTypes.STRING,
-		allowNull: false,
-	},
-	birthdate: {
-		type: DataTypes.DATEONLY, // FORMATO DE FECHA = Año/mes/día 
-		allowNull: false,
-	},
-	address: {
-		type: DataTypes.STRING,
-	},
-	email: {
-	 	type: DataTypes.STRING,
-	 	allowNull: false,
-	 	unique: true,
-	 	validate: {
-	 		isEmail: true,
-	 	},
-	},
-	userName: {
-		type: DataTypes.STRING,
-		allowNull: false,
-		unique: true,
-	},
-	password: {   // Usar funciones para hashear o encriptar, y también para verificar 
-		type: DataTypes.STRING,   // librería bcrypt
-		allowNull: false,
-	},
-	numberPhone: {
-		type: DataTypes.STRING,
-	},
-	typeUser: {
-		type: DataTypes.ENUM('Adopter', 'Admin'),
-		allowNull: false,
-	},
-	donante: {
-		type: DataTypes.BOOLEAN,
-		defaultValue: false,
-	},
-	{
-		freezeTableName: true
-	}
-});
+module.exports = (sequelize) => {
+	const User = sequelize.define('User', {
+		id: {
+			type: DataTypes.UUID,
+			primaryKey: true,
+			defaultValue: DataTypes.UUIDV4,
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		birthdate: {
+			type: DataTypes.DATEONLY, // FORMATO DE FECHA = Año/mes/día 
+			allowNull: false,
+		},
+		address: {
+			type: DataTypes.STRING,
+		},
+		email: {
+	 		type: DataTypes.STRING,
+	 		allowNull: false,
+	 		unique: true,
+	 		validate: {
+	 			isEmail: true,
+	 		},
+		},
+		userName: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+		},
+		password: { 
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		numberPhone: {
+			type: DataTypes.STRING,
+		},
+		typeUser: {
+			type: DataTypes.ENUM('Adopter', 'Admin'),
+			allowNull: false,
+		},
+		donante: {
+			type: DataTypes.BOOLEAN,
+			defaultValue: false,
+		},
+		{
+			freezeTableName: true
+		}});
+
+		// Esto hashea las contraseñas antes de que genere el registro o actualizar la contraseña
+		User.beforeCreate(async (user) => {
+			if(user.changed('password')) {
+				user.password = await hashPassword(user.password);
+			}
+		});
+
+		User.beforeUpdate(async (user) => {
+			if(user.changed('password')) {
+				user.password = await hashPassword(user.password);
+			}
+		});
+		// Verificación de contraseña
+		User.prototype.verifyPassword = async (password) => {
+			return await comparePassword(password, this.password);
+		};
+	return User;
+};
