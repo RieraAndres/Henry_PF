001a0f4eb898f7507b481ce59dbdc1e50079091d
¨JaviZaldu¨ <javizaldu9@gmail.com>
2023-08-03 17:17:44 -0300

    Desarrolo Login y Register Form

--- Client/src/Components/LoginForm/LoginForm.jsx
+++ Client/src/Components/LoginForm/LoginForm.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom"; // Para redireccionar a la ruta de inicio después del inicio de sesión
-
+import RegisterForm from "../RegisterForm/RegisterForm";
 import styles from "./LoginForm.module.css";
+
+
 function LoginForm() {
   const navigate = useNavigate();
 
@@ -26,6 +28,11 @@ function LoginForm() {
       errors.password = "Enter a password";
     }
     return errors;
+  }
+
+  const [showRegisterForm, setShowRegisterForm] = useState(false);
+  function handleCreateAccountClick() {
+    setShowRegisterForm(true);
   }
 
   const [user, setUser] = useState({
@@ -60,7 +67,9 @@ function LoginForm() {
   
 function handleSubmit(e) {
   e.preventDefault();
-  if (!errors.username && !errors.password) {
+  const formErrors = validate(user);
+  setErrors(formErrors);
+  if (Object.keys(formErrors).length === 0) {
     navigate("/inicio"); // redireccionamos a la ruta de inicio después del inicio de sesión.
   } else {
     alert("Datos incorrectos");
@@ -68,8 +77,12 @@ function handleSubmit(e) {
 }
 
   return (
-    <div className={styles.loginForm}>
-      <form>
+    <div>
+      {showRegisterForm ? (
+        <RegisterForm />
+      ) : (
+        // Mostrar el formulario de inicio de sesión si showRegisterForm es falso
+        <form className={styles.loginForm}>
         <h3>INICIAR SESIÓN</h3>
         <div className="mb-3">
           <label htmlFor="username" className="form-label">
@@ -77,12 +90,13 @@ function handleSubmit(e) {
           </label>
           <input
             type="text"
-            className="form-control"
+            className={`form-control ${errors.username ? styles.errorInput : ""}`}
             id="username"
             name="username"
             value={user.username}
             onChange={handleChange}
           />
+          {errors.username && <p className={styles.errorMsg}>{errors.username}</p>}
         </div>
         <div className="mb-3">
           <label htmlFor="password" className="form-label">
@@ -90,29 +104,31 @@ function handleSubmit(e) {
           </label>
           <input
             type="password"
-            className="form-control"
+            className={`form-control ${errors.password ? styles.errorInput : ""}`}
             id="password"
             name="password"
             value={user.password}
             onChange={handleChange}
           />
+          {errors.password && <p className={styles.errorMsg}>{errors.password}</p>}
         </div>
         <div>
           <button type="submit" onClick={handleSubmit} >
             Iniciar Sesión
           </button>
-          <button type="button">
+          <button type="button" onClick={handleCreateAccountClick}>
             Crear una cuenta
           </button>
         </div>
-      </form>
-      <div className={styles.socialLogin}>
+        <div className={styles.socialLogin}>
         <p>O inicia sesión con:</p>
         <div>
             <a href=""> <img src="https://img.freepik.com/iconos-gratis/google_318-258888.jpg" alt="googleLogo" /> </a>
             <a href=""> <img src= "https://img.freepik.com/iconos-gratis/facebook_318-157463.jpg" alt="facebookLogo" /> </a>
         </div>
       </div>
+      </form>
+      )}
     </div>
   );
 };
