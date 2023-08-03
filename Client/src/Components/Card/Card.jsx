ce0a9169d9f433e3be05de429fa8ea52edc1785f
Andres Riera <andresriera9999@gmail.com>
2023-08-03 16:23:58 -0300

    Link a detail en Card

--- Client/src/Components/Card/Card.jsx
+++ Client/src/Components/Card/Card.jsx
@@ -1,15 +1,23 @@
 import Card from 'react-bootstrap/Card';
 import styles from '../Card/Card.module.css'; // Importa los estilos del archivo CSS externo
+import { NavLink } from "react-router-dom";
+
 
 function CardComponent({ mascota }) { //traigo mascota desde Cards 
-  const { name, img } = mascota; //destructuro datos a renderizar en la Card
+  const { name, img ,id} = mascota; //destructuro datos a renderizar en la Card
   return (
-    <Card className={styles.cardContainer} >
-      <Card.Img className={styles.cardImage} variant="top" src={img} />
-      <Card.Body>
+    <div className={styles.cardContainer}>
+      <NavLink to={`${id}`} style={{textDecoration: 'none'}}>
+       <Card className={styles.cardData} >
+        <Card.Img className={styles.cardImage} variant="top" src={img} />
+        <Card.Body>
         <Card.Title>{name}</Card.Title>
-      </Card.Body>
-    </Card>
+        </Card.Body>
+       </Card>
+      </NavLink>
+    </div>
+    
+   
   );
 }
 
--- Client/src/Components/Cards/cards.jsx
+++ Client/src/Components/Cards/cards.jsx
@@ -1,18 +1,18 @@
 import Card from "../Card/Card"
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import styles from '../Cards/Cards.module.css'
+import styles from '../Cards/cards.module.css'
 function CardsComponent(/*{mascotas}*/) { //traigo las mascotas por props desde Home
   
   const mascotas = [
-    {name:"Pepito",img:"https://humanidades.com/wp-content/uploads/2017/02/perro-1-e1561678907722.jpg"},
-    {name:"Olaf",img:"https://upload.wikimedia.org/wikipedia/commons/0/04/Labrador_Retriever_%281210559%29.jpg"},
-    {name:"Floki",img:"https://t1.ea.ltmcdn.com/es/posts/5/6/2/10_caracteristicas_de_los_perros_24265_600_square.jpg"},
-    {name:"Floki",img:"https://t1.ea.ltmcdn.com/es/posts/5/6/2/10_caracteristicas_de_los_perros_24265_600_square.jpg"},
-    {name:"Floki",img:"https://t1.ea.ltmcdn.com/es/posts/5/6/2/10_caracteristicas_de_los_perros_24265_600_square.jpg"},
-    {name:"Floki",img:"https://t1.ea.ltmcdn.com/es/posts/5/6/2/10_caracteristicas_de_los_perros_24265_600_square.jpg"},
-    {name:"Floki",img:"https://t1.ea.ltmcdn.com/es/posts/5/6/2/10_caracteristicas_de_los_perros_24265_600_square.jpg"},
-    {name:"Floki",img:"https://t1.ea.ltmcdn.com/es/posts/5/6/2/10_caracteristicas_de_los_perros_24265_600_square.jpg"}
+    {id:1,name:"Pepito",img:"https://humanidades.com/wp-content/uploads/2017/02/perro-1-e1561678907722.jpg"},
+    {id:2,name:"Olaf",img:"https://upload.wikimedia.org/wikipedia/commons/0/04/Labrador_Retriever_%281210559%29.jpg"},
+    {id:3,name:"Floki",img:"https://t1.ea.ltmcdn.com/es/posts/5/6/2/10_caracteristicas_de_los_perros_24265_600_square.jpg"},
+    {id:4,name:"Floki",img:"https://t1.ea.ltmcdn.com/es/posts/5/6/2/10_caracteristicas_de_los_perros_24265_600_square.jpg"},
+    {id:5,name:"Floki",img:"https://t1.ea.ltmcdn.com/es/posts/5/6/2/10_caracteristicas_de_los_perros_24265_600_square.jpg"},
+    {id:6,name:"Floki",img:"https://t1.ea.ltmcdn.com/es/posts/5/6/2/10_caracteristicas_de_los_perros_24265_600_square.jpg"},
+    {id:7,name:"Floki",img:"https://t1.ea.ltmcdn.com/es/posts/5/6/2/10_caracteristicas_de_los_perros_24265_600_square.jpg"},
+    {id:8,name:"Floki",img:"https://t1.ea.ltmcdn.com/es/posts/5/6/2/10_caracteristicas_de_los_perros_24265_600_square.jpg"}
   ]
 return (
     <div className={styles.CardsContainer}>
