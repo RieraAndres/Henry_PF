644c6030ae20134ac650ba1b1483b8a13e069f52
Troceitor <alexisaguilarg19@gmail.com>
2023-08-05 15:43:17 -0400

    componenteFormAdopt

--- Client/src/Views/Detail/Detail.jsx
+++ Client/src/Views/Detail/Detail.jsx
@@ -1,10 +1,7 @@
-import FormAdopt from "../../Components/FormAdopt/FormAdopt";
-
 function Detail () {
     return(
         <div>
             <p>Estas en Detail</p>
-            <FormAdopt></FormAdopt>
         </div>
     )
 }
