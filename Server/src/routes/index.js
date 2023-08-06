3596fffa474f5abc97f4c63e89aeedea49cc9762
MarcosMoreno0912 <moreno83marcos@gmail.com>
2023-08-06 03:29:45 -0300

    CRUD Pets completado, agregué ruta para borrado lógico

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -7,12 +7,14 @@ const routes = Router();
 const routerPets = require('./routesPets')
 // const routerFilter = require('./routesFilter')
 const routerForUsers = require('./routesUser.js') //----Marcos
+const routerCloud = require('../cloudinary/imgPetsRoutes')
 // const {} = require('./routesDonations')
 // const {} = require('./routesAdoptions')
 
 routes.use('/mascotas', routerPets)  // Model : mascota
 // routes.use('/filter', routerFilter)
 routes.use('/usuario', routerForUsers) // Model : User ----Marcos
+routes.use('/cloud', routerCloud) // Ruta para la carga de imágenes de las mascotas en Cloudinary
 // routes.use('/donacion', /*rutaDonacion*/) // Model : Donacion
 // routes.use('/Adopcion', /*rutaAdopcion*/) // Model : Adopcion
 
