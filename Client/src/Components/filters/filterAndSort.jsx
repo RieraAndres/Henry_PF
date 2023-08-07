e8012c6d706ea87b0caa3215f4dea5afaa9cb958
Aguicarpy <aguicarpy@gmail.com>
2023-08-07 05:41:09 -0400

    componenteFiltroOK

---
+++ Client/src/Components/filters/filterAndSort.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Form, Row, Col, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter, setOrden, applyFilters } from '../../Redux/Actions';
+
+const FiltersComponent = () => {
+  const dispatch = useDispatch();
+  const { filters, orden } = useSelector((state) => state);
+
+  const handleFilterChange = (filterName, value) => {
+    dispatch(setFilter({ ...filters, [filterName]: value }));
+  };
+
+  const handleOrdenChange = (ordenName, value) => {
+    dispatch(setOrden({ ...orden, [ordenName]: value }));
+  };
+
+  const handleApplyFilters = () => {
+    dispatch(applyFilters(filters, orden));
+  };
+
+  return (
+    <Form>
+      <Row>
+        {/* <Col>
+          <Form.Control
+            type="text"
+            value={filters.name}
+            onChange={(e) => handleFilterChange('name', e.target.value)}
+            placeholder="Nombre"
+          />
+        </Col> */}
+        <Col>
+          <Form.Control
+            as="select"
+            value={filters.gender}
+            onChange={(e) => handleFilterChange('gender', e.target.value)}
+          >
+            <option value="">Género</option>
+            <option value="macho">Macho</option>
+            <option value="hembra">Hembra</option>
+          </Form.Control>
+        </Col>
+        <Col>
+          <Form.Control
+            as="select"
+            value={orden.orden_age}
+            onChange={(e) => handleOrdenChange('orden_age', e.target.value)}
+          >
+            <option value="">Ordenar por edad</option>
+            <option value="ASC">Ascendente</option>
+            <option value="DESC">Descendente</option>
+          </Form.Control>
+        </Col>
+        <Col>
+          <Form.Control
+            as="select"
+            value={orden.orden_name}
+            onChange={(e) => handleOrdenChange('orden_name', e.target.value)}
+          >
+            <option value="">Ordenar por nombre</option>
+            <option value="ASC">Ascendente</option>
+            <option value="DESC">Descendente</option>
+          </Form.Control>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button variant="primary" onClick={handleApplyFilters}>Filtrar</Button>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
+
+export default FiltersComponent;
+
+
--- Client/src/Redux/Reducer.js
+++ Client/src/Redux/Reducer.js
@@ -5,9 +5,24 @@ import {
   POST_PET_FAILURE,
   CLEAR_AUX_STATE,
   GET_PET_BY_NAME,
+  SET_FILTER,
+  SET_ORDEN,
+  APPLY_FILTERS_SUCCESS,
+  APPLY_FILTERS_FAILURE
 } from "./Actions";
 
-let initialState = { allPets: [], petsCopy: [], auxState: [] };
+let initialState = { 
+  allPets: [], 
+  petsCopy: [], 
+  auxState: [],
+  filters: { //filtros
+    // name: "",
+    gender: "",
+  },
+  orden: {
+    orden_age: "",
+    orden_name: "",
+}, };
 
 export default function rootReducer(state = initialState, action) {
   switch (action.type) {
@@ -45,6 +60,32 @@ export default function rootReducer(state = initialState, action) {
       return {
         ...state,
         auxState: [], // el estado debe setearse como vacío y no como null, porque eso provoca que se rompa el detail
+      };
+      case SET_FILTER: // Nuevo caso para setear los filtros
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          ...action.payload,
+        },
+      };
+    case SET_ORDEN: // Nuevo caso para setear los parámetros de orden
+      return {
+        ...state,
+        orden: {
+          ...state.orden,
+          ...action.payload,
+        },
+      };
+      case APPLY_FILTERS_SUCCESS:
+      return {
+        ...state,
+        petsCopy: action.payload, // Actualiza petsCopy con los datos filtrados y ordenados del servidor
+      };
+    case APPLY_FILTERS_FAILURE:
+      return {
+        ...state,
+        error: action.payload, // Maneja el error en caso de fallo
       };
 
     default:
