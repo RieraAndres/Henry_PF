a501f5a5ba2310e49d43b517f2371f5c2697ffa4
Micaela <micaescu99@gmail.com>
2023-08-09 12:53:39 -0300

    cambio las observaciones

--- Client/src/Views/About/About.jsx
+++ Client/src/Views/About/About.jsx
@@ -12,7 +12,7 @@ function Info () {
                     <p>En Patitas sin hogar, estamos comprometidos a encontrar un hogar lleno de amor, cariño y protección a las mascotas necesitadas de hogares<br /> 
                     Nuestra pasion por los animales y el deseo de crear un mundo donde cada mascota encuentre su lugar perfecto.</p>
                     <h3>NUESTRA MISIÓN</h3>
-                    <p>Es simple pero poderosa, brindar una segunda oportinidad a las mascotas abandonadas, maltratadas, o simplemente que no puedan seguir en su hogar,
+                    <p>Es simple pero poderosa, brindar una segunda oportunidad a las mascotas abandonadas, maltratadas, o simplemente que no puedan seguir en su hogar,
                         queremos conectarlos con familias cariñosas que esten dispuestas a darles un hogar. Creemos que cada vida animal es valiosa y merece ser tratada con respeto y compasión.
                     </p>
                     
@@ -20,7 +20,7 @@ function Info () {
                 <div className={styles.trabajo}>
                     <h3>¿COMO TRABAJAMOS?</h3>
                     <p><b>COLABORACIÓN:</b>Con refugios y organizaciones de rescate para proporcionar un refugio seguro y atención médica.</p>
-                    <p><b>ADOPCIÓN RESPONSABLE:</b> Se realiza un proseso de adopción para asegurar de que cada una sea adoptada por la familia adecuada.</p>
+                    <p><b>ADOPCIÓN RESPONSABLE:</b> Se realiza un proceso de adopción para asegurar de que cada una sea adoptada por la familia adecuada.</p>
                     <p><h5>GRACIAS POR VISITAR Patitas sin hogar.</h5></p>
                     <p>Esperamos que encuentres la compañia perfecta en uno de nuestros adorables amigos.
                         <b/>¡No dudes en contactarnos!</p>
