a6c2c48f9111f2ddb1684c035a770bfff16193fc
Aguicarpy <aguicarpy@gmail.com>
2023-08-09 20:14:29 -0400

    feature ruta para adoptar

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -3,25 +3,18 @@ const routes = Router();
 
 //Imports rutas segun el modelo
 const routerPets = require('./routesPets')
-
-// const {} = require('./routesUser')
-
-// const routerFilter = require('./routesFilter')
 const routerForUsers = require('./routesUser.js') //----Marcos
 const routerCloud = require('../cloudinary/imgPetsRoutes')
 // const {} = require('./routesDonations')
-// const {} = require('./routesAdoptions')
+
 
 routes.use('/mascotas', routerPets)  // Model : mascota
-
-
-// routes.use('/usuario', /*rutaUser*/)      // Model : User
-
-// routes.use('/filter', routerFilter)
 routes.use('/usuario', routerForUsers) // Model : User ----Marcos
 routes.use('/cloud', routerCloud) // Ruta para la carga de imágenes de las mascotas en Cloudinary
 
+
+
 // routes.use('/donacion', /*rutaDonacion*/) // Model : Donacion
-// routes.use('/Adopcion', /*rutaAdopcion*/) // Model : Adopcion
+
 
 module.exports = routes;
