91ed93cfab8b212f8a8eb1aa1446e6e764db0df3
Andres Riera <andresriera9999@gmail.com>
2023-08-11 12:37:06 -0300

    Ruta para traer data usuario

--- Server/src/controllers/crudUser/getProfileUser.js
+++ Server/src/controllers/crudUser/getProfileUser.js
@@ -1,1 +1,12 @@
-//Ruta que dirige al perfil del usuario
+const { User } = require ("../../db.js")
+
+const getUser = async (userName)=>{
+    try {
+        const user = await User.findOne({where:{userName:userName}})
+        return user;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+module.exports = getUser;
