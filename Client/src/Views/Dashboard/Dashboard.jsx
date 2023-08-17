7e91bd7d36dab6498d3bdb3c980c10075f897b19
Andres Riera <andresriera9999@gmail.com>
2023-08-17 12:24:07 -0300

    Creo view de dashboard

---
+++ Client/src/Views/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+// import styles from "./ErrorPage.module.css"
+
+const Dashboard = ({access}) => {
+
+  return (
+   <div>
+    <p>Estas en Dashboard</p>
+   </div>
+  );
+};
+
+export default Dashboard;
