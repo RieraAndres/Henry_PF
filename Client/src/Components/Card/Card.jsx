dff51619f1aeecd7655f9dae6122ab5410c13d48
Andres Riera <andresriera9999@gmail.com>
2023-08-21 17:45:29 -0300

    Doy estilos al boton de eliminar mascota

--- Client/src/Components/Card/Card.jsx
+++ Client/src/Components/Card/Card.jsx
@@ -51,7 +51,11 @@ function CardComponent({ mascota }) { //traigo mascota desde Cards
               <p style={{fontSize:"30px"}}>{name}</p>
               <p style={{fontSize:"30px"}}>{age} años</p>
               {status === true?(<p style={{fontSize:"30px"}}>Activa</p>):(<p style={{fontSize:"30px"}}>Inactiva</p>)}
-              <button onClick={()=>handleDeletePet(id)}>Eliminar</button>
+              <button className={styles.deletebutton} onClick={()=>handleDeletePet(id)} >
+                <svg className={styles.deletesvgIcon} viewBox="0 0 448 512">
+                <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
+                </svg>
+              </button>
               {showAlert && 
                <Alert key='success' variant='success' style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 9999 }}>{alert}</Alert>
               }
